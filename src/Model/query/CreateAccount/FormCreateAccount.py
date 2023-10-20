d8f91e165a2f4774722b0bb93a0aa2974b747263
lucascampos04 <camposdlucasoli@gmail.com>
2023-10-20 16:53:49 -0300

    Consulta de criação de conta

---
+++ src/Model/query/CreateAccount/FormCreateAccount.py
@@ -0,0 +1,27 @@
+from src.Model.database.connect import connect_database, close_database
+
+def query_form_create_account(name, rg, cpf, email, telefone):
+    connect = connect_database()
+    if connect is not None:
+        try:
+            cursor = connect.cursor()
+            query = """
+                INSERT INTO logs_users(nome, rg, cpf, email, telefone)
+                VALUES (%s, %s, %s, %s, %s)
+            """
+            cursor.execute(query, (name, rg, cpf, email, telefone))
+            result = cursor.fetchone()
+
+            if result:
+                print("Usuario cadastrado com sucesso")
+                return result
+            else:
+                print("Houve erro")
+                return None
+        except Exception as e:
+            print(f"Erro durante a criação de usuario :  {str(e)}")
+        finally:
+            close_database(connect)
+    else:
+        print("Não foi possível conectar ao banco de dados.")
+
